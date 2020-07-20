6fd99b5984d9d5d8d8c66a0f20f9326e9d24ce19
aem14 <63332907+aem14@users.noreply.github.com>
2020-07-19 19:19:35 -0500

    200719
    Grid Search w/Plots of 2D Power Leakage graph and 3D Plots of the coefficients.

--- example.m
+++ example.m
@@ -1,16 +1,12 @@
 clear; clc; close all;
 %% Setup Everything
-
 % Add the submodules to path
 addpath(genpath('OFDM-Matlab'))
 addpath(genpath('WARPLab-Matlab-Wrapper'))
 addpath(genpath('Power-Amplifier-Model'))
-
 rms_input = 0.50;
-
 % Setup the PA simulator or TX board
 PA_board = 'webRF'; % either 'WARP', 'webRF', or 'none'
-
 switch PA_board
     case 'WARP'
         warp_params.nBoards = 1;         % Number of boards
@@ -21,10 +17,9 @@ switch PA_board
         board = PowerAmplifier(7, 4);
         Fs = 40e6;    % WARP board sampling rate.
     case 'webRF'
-        dbm_power = -24; % Originally -22
+        dbm_power = -22;
         board = webRF(dbm_power);
 end
-
 % Setup OFDM
 ofdm_params.nSubcarriers = 1200;
 ofdm_params.subcarrier_spacing = 15e3; % 15kHz subcarrier spacing
@@ -32,12 +27,10 @@ ofdm_params.constellation = 'QPSK';
 ofdm_params.cp_length = 144; % Number of samples in cyclic prefix.
 ofdm_params.nSymbols = 14;
 modulator = OFDM(ofdm_params);
-
 % Create TX Data
 [tx_data, ~] = modulator.use;
 tx_data = Signal(tx_data, modulator.sampling_rate, rms_input);
 tx_data.upsample(board.sample_rate)
-
 % Setup DPD
 dpd_params.order = 3;
 dpd_params.memory_depth = 1;
@@ -49,164 +42,128 @@ dpd_params.use_even = false;
 dpd_params.use_conj = 0;    % Conjugate branch. Currently only set up for MP (lag = 0)
 dpd_params.use_dc_term = 0; % Adds an additional term for DC
 dpd = ILA_DPD(dpd_params);
-
 %% Run Experiment
 [~, w_out_dpd] = board.transmit(tx_data.data);
 dpd.perform_learning(tx_data.data, board);
 
-[~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-after = w_dpd.measure_all_powers;
+% Loop over coefficients here
+shifts = [-0.05 + 0.05j, 0.05, 0.05, -0.1 - 0.05j, 0.05, 0.05, -0.1 - 0.05j, 0.05, 0.05]'; % A somewhat odd way to move coefficients in a square
 
-after_values = [after(1,1)];
-total_gradient = inf;
+% Move coefficients in a square grid, from left to right going from row by row
+% [1]      [2]       [3] 
+% [4] [5 (original)] [6] 
+% [7]      [8]       [9]
 
-while total_gradient > 0.005 % Iterates until the sum of the gradient vector is less than 0.005
-    % Shift in R^2 space
-    del = 0.05 + 0.05i; 
-    coeff_length = length(dpd.coeffs);
-    % Initialize an empty gradient_vector
-    gradient_vector = zeros(coeff_length,1);
-    for i = 1:coeff_length % Fills in each value in the gradient vector
-        original_coeffs = dpd.coeffs;
-        plus_delta = dpd.coeffs;
-        minus_delta = dpd.coeffs;
-        
-        % Plus_delta is the shift in the positive direction in R^2 whereas
-        % minus_delta is shift in the negative direction
-        plus_delta(i, 1) = plus_delta(i, 1) + del;
-        minus_delta(i, 1) = minus_delta(i, 1) - del;
-        
-        %Ideally I would just make a new dpd object, but since I'm not sure
-        %about the specifics, here I'm just resetting the dpd.coeffs to be
-        %whatever I want them to be, transmitting that, and then restoring
-        %dpd.coeffs back to what they were originally. It's a bit rachet,
-        %but I'm not entirely sure of a better way to do it
-        dpd.coeffs = plus_delta;
-        [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-        after = w_dpd.measure_all_powers;
-        plus_delta = after(1,1);
-        
-        dpd.coeffs = minus_delta;
-        [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-        after = w_dpd.measure_all_powers;
-        minus_delta = after(1,1);
-        
-        dpd.coeffs = original_coeffs;
-        gradient_vector(i, 1) = (plus_delta - minus_delta)/sqrt(.02);
-    end
-    
-    % This creates the Hessian Matrix
-    hess_dim = length(dpd.coeffs);
-    hessian_grid = zeros(hess_dim, hess_dim);
-    
-    % Filling in the Hessian Matrix
-    for i = 1:hess_dim 
-        for j = 1:hess_dim
-            original_coeffs = dpd.coeffs;
-            % Filling in the second derivatives along the diagonal
-            if i == j
-                plus_del = dpd.coeffs;
-                zero_del = dpd.coeffs;
-                minus_del = dpd.coeffs;
-                plus_del(i, 1) = plus_del(i, 1) + del;
-                minus_del(i, 1) = minus_del(i, 1) - del;
-                
-                % Once again, I don't know how to do this other than just
-                % redefining dpd.coeffs, transmitting that, and then
-                % resetting dpd.coeffs back to the original one
-                dpd.coeffs = plus_del;
-                [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-                after = w_dpd.measure_all_powers;
-                plus_del_out = after(1,1);
-                
-                dpd.coeffs = zero_del;
-                [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-                after = w_dpd.measure_all_powers;
-                zero_del_out = after(1,1);
-                
-                dpd.coeffs = minus_del;
-                [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-                after = w_dpd.measure_all_powers;
-                minus_del_out = after(1,1);
-                
-                % Reset dpd.coeffs back to the original coeffs
-                dpd.coeffs = original_coeffs;
-                % Set the value in the Hessian Matrix
-                hessian_grid(i, j) = (plus_del_out - 2 * zero_del_out + minus_del_out) / (del^2);
-            
-            % For all other values not on the diagonal
-            else
-                % If you look at the formula I wrote out, I have to pass
-                % different values into the "function", which in this case
-                % is just transmitting the dpd object and observing the
-                % output spectrum power. This is a way I figured out how to
-                % do it, but I'll see if it's possible to simplify it down
-                % a bit.
-                copy1 = dpd.coeffs;
-                copy2 = dpd.coeffs;
-                copy3 = dpd.coeffs;
-                copy4 = dpd.coeffs;
-                copy1(j, 1) = copy1(j, 1) + del;
-                copy1(i, 1) = copy1(i, 1) + del;
-                copy2(j, 1) = copy2(j, 1) - del;
-                copy2(i, 1) = copy2(i, 1) + del;
-                copy3(j, 1) = copy3(j, 1) + del;
-                copy3(i, 1) = copy3(i, 1) - del;
-                copy4(j, 1) = copy4(j, 1) - del;
-                copy4(i, 1) = copy4(i, 1) - del;
-                
-                % Transmitting the four different vectors
-                dpd.coeffs = copy1;
-                [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-                after = w_dpd.measure_all_powers;
-                copy1_out = after(1,1);
-                
-                dpd.coeffs = copy2;
-                [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-                after = w_dpd.measure_all_powers;
-                copy2_out = after(1,1);
-                
-                dpd.coeffs = copy3;
-                [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-                after = w_dpd.measure_all_powers;
-                copy3_out = after(1,1);
-                
-                dpd.coeffs = copy4;
-                [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-                after = w_dpd.measure_all_powers;
-                copy4_out = after(1,1);
-                
-                % Reset dpd.coeffs back to the original coeffs
-                dpd.coeffs = original_coeffs;
-                % Set the value in the Hessian Matrix
-                hessian_grid(i, j) = (copy1_out - copy2_out - copy3_out - copy4_out) / (4 * (del ^ 2));
-            end
+shifts = [-0.05 + 0.05j, 0.05, 0.05, -0.1 - 0.05j, 0.05, 0.05, -0.1 - 0.05j, 0.05, 0.05]'; 
+% all_coeffs_cell holds all coefficients along with the 'after' values to
+% be plotted later
+all_coeffs_cell = {};
+prec_iter = 0;
+precision = 4;
+% If the minimum value is at the previously found coefficient, decrease the grid by half.
+% 'precision' is a limit as to how many times this grid is cut in half
+precision = 2; 
+
+while prec_iter < precision
+    % all_coeffs holds just the current grid 
+    all_coeffs = [];
+
+    for k1 = 1:length(shifts)
+        for b=1:length(dpd.coeffs)
+            % Loop through dpd.coeffs and apply a shift
+            dpd.coeffs(b, 1) = dpd.coeffs(b, 1) + shifts(k1, 1);
         end
+
+
+        % Transmit
+        [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
+        before = w_out_dpd.measure_all_powers;
+        after = w_dpd.measure_all_powers;
+
+
+        % Make copies
+        inter_coeffs = dpd.coeffs;
+        after_coeffs = after;
+
+
+        % Append the leftmost value of after_coeff to the vector containing
+        % the coefficients
+        inter_coeffs = [inter_coeffs; after_coeffs(1,1)];
+
+        % Now append this vector containing the coefficients with their
+        % corresponding after value to all_coeffs. (This vector is reset in
+        % every loop and is only used to compare 'after' values)
+        all_coeffs = [all_coeffs, inter_coeffs];
+
+        % Also append this vector to a cell vector used to make the plot at
+        % the end
+        all_coeffs_cell = [all_coeffs_cell, num2cell(inter_coeffs)];
     end
-    inv_hess = inv(hessian_grid);
-    
-    % See how much optimizer is changing the dpd.coeff vector by
-    new_vect = dpd.coeffs - (inv_hess * gradient_vector);
-    disp(new_vect - dpd.coeffs);
-    
-    %xk+1 = xk - [Hf(xk)]^-1 * gradient(f(xk)) 
-    dpd.coeffs = dpd.coeffs - (inv_hess * gradient_vector);
-    
-    total_gradient = sum(gradient_vector);
-    
-    % Add to the after_values vector to tell whether model is actually
-    % optimizing
-    [~, w_dpd] = board.transmit(dpd.predistort(tx_data.data));
-    after_lvalue = w_dpd.measure_all_powers;
-    after_values = [after_values, after_lvalue(1,1)]; 
 
+    disp(all_coeffs)
+
+    % Find the minimum after value in the grid of 9 coefficient/after
+    % vectors in all_coeffs
+    [m, I] = min(all_coeffs(3, :));
+    small = all_coeffs(:, I);
+    disp(I)
+
+    % If the coefficients corresponding to the smallest 'after' value is
+    % the 5th/center value (aka the 'current' coefficient), then divide all
+    % the shifts by half, increase prec_iter, and rerun the loop
+    if I == 5
+        for d=1:length(shifts)
+            shifts(d) = shifts(d)/2;
+        end
+        prec_iter = prec_iter + 1;
+        fprintf ("prec iter is: %d\n", prec_iter)
+    end
+
+    % Change current coefficient to the ones corresponding to the smallest
+    % 'after' value
+    for c=1:length(dpd.coeffs)
+        dpd.coeffs(c,1) = all_coeffs(c, I);
+    end
 end
+disp(all_coeffs)
 
-disp(after_values);
+% Plotting code
 
+x_axis = {};
+y_axis = {};
+z_axis = {};
+for order_iter = 1:length(dpd.coeffs)
+    thisfig = figure();
+    x_axis = {};
+    y_axis = {};
+    z_axis = {};
+    for shift_iter = 1:length(all_coeffs_cell)
+        x_axis = [x_axis, real(all_coeffs_cell{order_iter, shift_iter})];
+        y_axis = [y_axis, imag(all_coeffs_cell{order_iter, shift_iter})];
+        z_axis = [z_axis, all_coeffs_cell{length(dpd.coeffs) + 1, shift_iter}];
+    end
+    x_axis = cell2mat(x_axis);
+    y_axis = cell2mat(y_axis);
+    z_axis = cell2mat(z_axis);
+    disp(x_axis);
+    disp(y_axis);
+    disp(z_axis);
+    plot3(x_axis, y_axis, z_axis, 'o');
+    xlabel('Real')
+    ylabel('Complex')
+    zlabel('after')
+    if order_iter == 1
+        title('1st coefficient');
+    elseif order_iter == 2
+        title('2nd coefficient');
+    elseif order_iter == 3
+        title('3rd coefficient');
+    else
+        title(strcat(num2str(order_iter), 'th coefficient'));
+    end
+    grid on
+end
 %% Plot
         w_out_dpd.plot_psd;
         w_dpd.plot_psd;
         dpd.plot_history;
-
-
